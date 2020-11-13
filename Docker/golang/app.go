8e4a10e2c7c1ff636890747113d61b325225a3c2
Bryan Echeverria <bryansyng2@gmail.com>
2020-11-13 17:18:22 -0600

    se agrego la carpeta docker

---
+++ Docker/golang/app.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/streadway/amqp"
+	"strconv"
+	"github.com/gorilla/mux"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+)
+
+type Caso struct {
+	Name string `json:"name"`
+	Location string `json:"location"`
+	Age int16 `json:"age"`
+	InfectedType string `json:"infectedType"`
+	State string `json:"state"`
+}
+
+func failOnError(err error, msg string){
+	if err != nil {
+		log.Fatalf(msg, err)
+	}
+}
+
+func addCaso(w http.ResponseWriter, r *http.Request){
+
+	//recuperar datos
+	var newCaso Caso
+	reqbody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Insert a valid case")
+	}
+	json.Unmarshal(reqbody, &newCaso)
+	name:= newCaso.Name
+	location := newCaso.Location
+	age := newCaso.Age
+	it := newCaso.InfectedType
+	state := newCaso.State
+
+	conn, err := amqp.Dial("amqp://guest:guest@0.0.0.0:5672/")
+	failOnError(err, "Fallo al conectar con rabbitmq")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Fallo al abrir el canal")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", //nombre de la cola a la que nos queremos susbcribir
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	failOnError(err, "Fallo al abrir el canal")
+
+	body := fmt.Sprintf("%s,%s;%s,%s;%s,%s;%s,%s;%s,%s;" , "name", name,"location", location,"age", strconv.Itoa(int(age)),"infectedType", it,"state", state)
+	err = ch.Publish(
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body: []byte(body),
+		})
+	log.Printf("Sent %s", body)
+	failOnError(err, "Fallo en enviar el mensaje")
+	
+}
+
+func main(){
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", addCaso).Methods("POST")
+	fmt.Println("El servidor go a la escucha en puerto 5000")
+	http.ListenAndServe(":5000", router)
+}
+
---
+++ Docker/python/receptor.py
@@ -0,0 +1,43 @@
+import pika
+from pymongo import MongoClient
+import redis
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+#---------------configuraciones de mongo-----------------
+#mongoClient =  MongoClient('35.225.245.55', 27017)
+mongoClient =  MongoClient('mongodb://35.225.245.55:27017/')
+db = mongoClient['proyecto']
+colleccion = db.casos
+#---------------configuraciones de redis-----------------
+redisClient = redis.Redis(host = '35.224.140.76', port = 6379)
+
+def callback(ch, method, properties, body):
+	cadena = str(body)[2:]
+	cadena = cadena[:len(cadena)-1]
+	parametros = cadena.split(";")
+	nombre = parametros[0].split(",")[1]
+	location = parametros[1].split(",")[1]
+	age = parametros[2].split(",")[1]
+	it = parametros[3].split(",")[1]
+	state = parametros[4].split(",")[1]
+	print("Nombre: " + nombre )
+	print("Location: " + location )
+	print("Age: " + age )
+	print("InfectedType: " + it )
+	print("State: " + state )
+	print("")
+	colleccion.insert({
+		"nombre" : nombre,
+		"location" : location,
+		"age" : age,
+		"infectedType" : it,
+		"state" : state
+	})
+	redisClient.sadd('casos', nombre+','+location+','+str(age)+',' + it + ',' + state)
+
+channel.basic_consume(queue = 'hello', on_message_callback = callback, auto_ack = True )
+print('Esperando por mensajes')
+channel.start_consuming()
